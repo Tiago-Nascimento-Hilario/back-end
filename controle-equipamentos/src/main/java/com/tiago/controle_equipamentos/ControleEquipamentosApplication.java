f0796a6af1de182f3ee158918defc0ca7caf0cfc
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2025-06-22 00:39:32 -0300

    criado Homecontroller e index.html

---
+++ controle-equipamentos/src/main/java/com/tiago/controle_equipamentos/ControleEquipamentosApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.controle_equipamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEquipamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEquipamentosApplication.class, args);
+	}
+
+}
